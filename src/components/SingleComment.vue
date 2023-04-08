147d44d4f81cf54b22260268a0d77468108b7aed
Jens Buehl <jbuehl@itemis.com>
2023-04-08 21:20:46 +0200

    Delete comment implemented

--- src/components/SingleComment.vue
+++ src/components/SingleComment.vue
@@ -4,7 +4,22 @@
       <v-col cols="10">{{ comment }}</v-col>
       <v-row justify="end">
         <v-col cols="auto" class="fill-height mr-1">
-          <v-btn v-if="authorId == user?.uid" color="primary" variant="text" block icon="mdi-delete"></v-btn>
+          <v-btn v-if="authorId == user?.uid" color="primary" variant="text" block icon="mdi-delete" @click="dialog = true"
+            ></v-btn>
+            <v-dialog v-model="dialog" width="auto">
+              <v-card rounded="lg" class="text-center primary">
+                <v-card-title>Delete Comment</v-card-title>
+                <v-card-text>
+                  Do you really want to delete this comment?<br />
+                  The action cannot be undone.
+                </v-card-text>
+                <v-card-actions>
+                  <v-btn color="error" block @click="handleDelete"
+                    >Delete</v-btn
+                  >
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
         </v-col>
       </v-row>
       <v-row justify="end">
@@ -34,30 +49,43 @@
 </template>
 
 <script>
-import { computed } from "vue";
+import { ref, computed } from "vue";
 import { useStore } from "vuex";
 import useTimeSince from "../composables/useTimeSince";
+import useCollection from "../composables/useCollection";
 
 export default {
   name: "SingleComment",
   props: ["comment"],
-  setup(props) {
+  emits: ["commentRemoved"],
+  setup(props, context) {
     const store = useStore();
     const user = computed(() => store.state.user);
+    const { del } = useCollection("comments");
 
+    const id = props.comment.id;
     const comment = props.comment.text;
     const timeCreated = props.comment.timeCreated;
     const author = props.comment.author;
     const authorId = props.comment.authorId;
+    const dialog = ref(false);
     const { timeSince, isNew } = useTimeSince();
+
+    const handleDelete = async () => {
+      dialog.value = false;
+      context.emit("commentRemoved"); 
+      await del(id);
+    };
 
     return {
       comment,
       timeCreated,
       author,
       authorId,
+      dialog,
       timeSince,
       isNew,
+      handleDelete,
       user
     };
   },
