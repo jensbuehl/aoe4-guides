71b7d04356dc6bbe7da69bccdd8931e3a07ee2a6
Jens Buehl <jbuehl@itemis.com>
2023-04-10 22:29:26 +0200

    Overlay conversion service

---
+++ src/composables/useOverlayConversion.js
@@ -0,0 +1,65 @@
+export default function useOverlayConversion() {
+  const convertToOverlayFormat = (build) => {
+    const overlay_steps = build.steps?.map((step) =>
+      convertStepToOverlayFormat(step)
+    );
+
+    return {
+      civilization: mapCivilizations[build.civ],
+      name: build.title,
+      author: build.author,
+      source: "undefined", //not supported
+      build_order: overlay_steps,
+    };
+  };
+
+  const convertStepToOverlayFormat = (step) => {
+    return {
+      age: -1, //not supported
+      population_count: -1, //not supported
+      time: step.time,
+      villager_count: parseInt(step.villagers) || -1,
+      resources: {
+        food: parseInt(step.food) || -1,
+        wood: parseInt(step.wood) || -1,
+        gold: parseInt(step.gold) || -1,
+        stone: parseInt(step.stone) || -1,
+      },
+      builders: parseInt(step.builders) || -1,
+      notes: [step.description],
+    };
+  };
+
+  const copyToClipboard = (text) => {
+    navigator.permissions.query({ name: "clipboard-write" }).then((result) => {
+      if (result.state === "granted") {
+        const type = "text/plain";
+        const blob = new Blob([text], { type });
+        let data = [new ClipboardItem({ [type]: blob })];
+        navigator.clipboard.write(data).then(
+          function () {
+            console.log("Copied to clipboard successfully!");
+          },
+          function (err) {
+            console.error("Unable to write to clipboard.", err);
+          }
+        );
+      }
+    });
+  };
+
+  const mapCivilizations = {
+    ENG: "English",
+    FRE: "French",
+    RUS: "Rus",
+    MAL: "Malians",
+    DEL: "Delhi Sultanate",
+    HRE: "Holy Roman Empire",
+    ABB: "Abbasid Dynasty",
+    OTT: "Ottomans",
+    CHI: "Chinese",
+    MON: "Mongols",
+  };
+
+  return { convertToOverlayFormat, copyToClipboard };
+}
