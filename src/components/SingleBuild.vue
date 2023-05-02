35452f23c0bb9095e212ddd91ef8f0d850024165
Jens Buehl <jbuehl@itemis.com>
2023-05-02 19:45:59 +0200

    Add min height depending on breakpoint, fix non-scaling civ image on medium width

--- src/components/SingleBuild.vue
+++ src/components/SingleBuild.vue
@@ -1,105 +1,166 @@
 <template>
-  <v-card class="d-flex align-center mb-4" rounded="lg">
-    <v-col v-if="build.civ" cols="3" class="pa-0 ma-0">
-      <v-img
-        :src="
-          '/' +
-          civs.find((item) => {
-            return item.shortName === build.civ;
-          }).flagLarge
-        "
-        :lazy-src="
-          '/' +
-          civs.find((item) => {
-            return item.shortName === build.civ;
-          }).flagSmall
-        "
-        gradient="to right, transparent, #222222"
-        alt="{{build.civ}}"
-        cover
-      >
-        <template v-slot:placeholder>
-          <v-row class="fill-height ma-0" align="center" justify="center">
-            <v-progress-circular
-              indeterminate
-              color="grey lighten-5"
-            ></v-progress-circular>
-          </v-row>
-        </template>
-      </v-img>
-    </v-col>
-    <v-col v-if="!build.civ" cols="3" class="pa-0 ma-0">
-      <v-img
-        src="/assets/flags/any-large.png"
-        lazy-src="/assets/flags/any-small.png"
-        gradient="to right, transparent, #222222"
-        alt="{{build.civ}}"
-        cover
-      >
-        <template v-slot:placeholder>
-          <v-row class="fill-height ma-0" align="center" justify="center">
-            <v-progress-circular
-              indeterminate
-              color="grey lighten-5"
-            ></v-progress-circular>
-          </v-row>
-        </template>
-      </v-img>
-    </v-col>
-    <v-col>
-      <div
-        class="text-subtitle-2 hidden-md-and-up"
-        style="font-family: 'Segoe UI' !important"
-      >
-        {{ build.title }}
-      </div>
-      <v-card-title class="pt-0 hidden-sm-and-down">
-        {{ build.title }}
-      </v-card-title>
-      <v-item-group class="ml-md-4 hidden-sm-and-down">
-        <v-chip
-          class="mr-2 mb-2"
-          v-if="isNew(build.timeCreated.toDate())"
-          label
-          color="primary"
-          size="small"
-          ><v-icon start icon="mdi-alert-decagram"></v-icon>NEW</v-chip
+  <v-card class="d-flex align-center mb-4" rounded="lg" :min-height=height>
+    <v-row no-gutters class="fill-height" align="center" justify="center">
+      <v-col v-if="build.civ" cols="3" class="pa-0 ma-0">
+        <v-img
+        :min-height=height
+          :src="
+            '/' +
+            civs.find((item) => {
+              return item.shortName === build.civ;
+            }).flagLarge
+          "
+          :lazy-src="
+            '/' +
+            civs.find((item) => {
+              return item.shortName === build.civ;
+            }).flagSmall
+          "
+          gradient="to right, transparent, #222222"
+          alt="{{build.civ}}"
+          cover
         >
-        <v-chip class="mr-2 mb-2" label size="small"
-          ><v-icon start icon="mdi-account-edit"></v-icon
-          >{{ build.author }}</v-chip
+          <template v-slot:placeholder>
+            <v-row class="fill-height ma-0" align="center" justify="center">
+              <v-progress-circular
+                indeterminate
+                color="grey lighten-5"
+              ></v-progress-circular>
+            </v-row>
+          </template>
+        </v-img>
+      </v-col>
+      <v-col v-if="!build.civ" cols="3" class="pa-0 ma-0">
+        <v-img
+        :min-height=height
+          src="/assets/flags/any-large.png"
+          lazy-src="/assets/flags/any-small.png"
+          gradient="to right, transparent, #222222"
+          alt="{{build.civ}}"
+          cover
         >
-        <v-chip
-          class="mr-2 mb-2"
-          v-if="build.timeCreated && orderBy == 'timeCreated'"
-          label
-          size="small"
-          ><v-icon start icon="mdi-clock-edit-outline"></v-icon
-          >{{ timeSince(build.timeCreated.toDate()) }}</v-chip
+          <template v-slot:placeholder>
+            <v-row class="fill-height ma-0" align="center" justify="center">
+              <v-progress-circular
+                indeterminate
+                color="grey lighten-5"
+              ></v-progress-circular>
+            </v-row>
+          </template>
+        </v-img>
+      </v-col>
+      <v-col>
+        <!--small title-->
+        <div
+          class="pb-1 ml-4 text-subtitle-2 hidden-lg-and-up"
+          style="font-family: 'Segoe UI' !important"
         >
-        <v-chip
-          class="mr-2 mb-2"
-          label
-          size="small"
-          v-show="build.views && orderBy == 'views'"
-        >
-          <v-icon start icon="mdi-eye"></v-icon>{{ build.views }}</v-chip
-        >
-        <v-chip v-show="build.likes && orderBy == 'likes'" class="mr-2 mb-2" label size="small">
-              <v-icon start icon="mdi-heart"></v-icon>
-              {{ build.likes }}</v-chip
-            >
-        <v-chip v-if="build.season" class="mr-2 mb-2" label size="small">{{
-          build.season
-        }}</v-chip>
-        <v-chip v-if="build.map" class="mr-2 mb-2" label size="small">{{
-          build.map
-        }}</v-chip>
-        <v-chip v-if="build.strategy" class="mr-2 mb-2" label size="small">{{
-          build.strategy
-        }}</v-chip>
-      </v-item-group>
-    </v-col>
+          {{ build.title }}
+        </div>
+        <!--small chips-->
+        <v-item-group class="ml-4 pt-1 hidden-sm-and-down hidden-lg-and-up">
+          <v-chip
+            class="mr-2"
+            v-if="isNew(build.timeCreated.toDate())"
+            label
+            color="primary"
+            size="x-small"
+            ><v-icon start icon="mdi-alert-decagram"></v-icon>NEW</v-chip
+          >
+          <v-chip class="mr-2" label size="x-small"
+            ><v-icon start icon="mdi-account-edit"></v-icon
+            >{{ build.author }}</v-chip
+          >
+          <v-chip
+            class="mr-2"
+            v-if="build.timeCreated && orderBy == 'timeCreated'"
+            label
+            size="x-small"
+            ><v-icon start icon="mdi-clock-edit-outline"></v-icon
+            >{{ timeSince(build.timeCreated.toDate()) }}</v-chip
+          >
+          <v-chip
+            class="mr-2"
+            label
+            size="x-small"
+            v-show="build.views && orderBy == 'views'"
+          >
+            <v-icon start icon="mdi-eye"></v-icon>{{ build.views }}</v-chip
+          >
+          <v-chip
+            v-show="build.likes && orderBy == 'likes'"
+            class="mr-2"
+            label
+            size="x-small"
+          >
+            <v-icon start icon="mdi-heart"></v-icon>
+            {{ build.likes }}</v-chip
+          >
+          <v-chip v-if="build.season" class="mr-2" label size="x-small">{{
+            build.season
+          }}</v-chip>
+          <v-chip v-if="build.map" class="mr-2" label size="x-small">{{
+            build.map
+          }}</v-chip>
+          <v-chip v-if="build.strategy" class="mr-2" label size="x-small">{{
+            build.strategy
+          }}</v-chip>
+        </v-item-group>
+        <!--large title-->
+        <v-card-title class="pt-0 pb-2 hidden-md-and-down">
+          {{ build.title }}
+        </v-card-title>
+        <!--large chips-->
+        <v-item-group class="ml-md-4 pt-2 hidden-md-and-down">
+          <v-chip
+            class="mr-2 mb-2"
+            v-if="isNew(build.timeCreated.toDate())"
+            label
+            color="primary"
+            size="small"
+            ><v-icon start icon="mdi-alert-decagram"></v-icon>NEW</v-chip
+          >
+          <v-chip class="mr-2 mb-2" label size="small"
+            ><v-icon start icon="mdi-account-edit"></v-icon
+            >{{ build.author }}</v-chip
+          >
+          <v-chip
+            class="mr-2 mb-2"
+            v-if="build.timeCreated && orderBy == 'timeCreated'"
+            label
+            size="small"
+            ><v-icon start icon="mdi-clock-edit-outline"></v-icon
+            >{{ timeSince(build.timeCreated.toDate()) }}</v-chip
+          >
+          <v-chip
+            class="mr-2 mb-2"
+            label
+            size="small"
+            v-show="build.views && orderBy == 'views'"
+          >
+            <v-icon start icon="mdi-eye"></v-icon>{{ build.views }}</v-chip
+          >
+          <v-chip
+            v-show="build.likes && orderBy == 'likes'"
+            class="mr-2 mb-2"
+            label
+            size="small"
+          >
+            <v-icon start icon="mdi-heart"></v-icon>
+            {{ build.likes }}</v-chip
+          >
+          <v-chip v-if="build.season" class="mr-2 mb-2" label size="small">{{
+            build.season
+          }}</v-chip>
+          <v-chip v-if="build.map" class="mr-2 mb-2" label size="small">{{
+            build.map
+          }}</v-chip>
+          <v-chip v-if="build.strategy" class="mr-2 mb-2" label size="small">{{
+            build.strategy
+          }}</v-chip>
+        </v-item-group>
+      </v-col>
+    </v-row>
   </v-card>
 </template>
 
@@ -107,7 +168,8 @@
 import getCivs from "../composables/getCivs";
 import useTimeSince from "../composables/useTimeSince";
 import { useStore } from "vuex";
-import { computed, onMounted } from "vue";
+import { computed, getCurrentInstance } from "vue";
+import { useDisplay } from 'vuetify'
 
 export default {
   name: "SingleBuild",
@@ -115,16 +177,25 @@ export default {
   setup() {
     const civs = getCivs().civs;
     const store = useStore();
+    const { name } = useDisplay()
     const orderBy = computed(() => store.state.filterConfig.orderBy);
     const { timeSince, isNew } = useTimeSince();
 
-    onMounted(() => {
-      console.log(orderBy.value);
-    });
+    const height = computed(() => {
+        switch (name.value) {
+          case 'xs': return 60
+          case 'sm': return 60
+          case 'md': return 60
+          case 'lg': return 112
+          case 'xl': return 112
+          case 'xxl': return 112
+        }
+      })
 
     return {
       civs,
       orderBy,
+      height,
       timeSince,
       isNew,
     };
