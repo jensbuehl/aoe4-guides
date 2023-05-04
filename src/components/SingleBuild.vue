240d52fd0a3c31c9821af27f1f226d1b410b9d1c
Jens Buehl <jbuehl@itemis.com>
2023-05-04 21:30:32 +0200

    Hide chips when no likes

--- src/components/SingleBuild.vue
+++ src/components/SingleBuild.vue
@@ -1,9 +1,9 @@
 <template>
-  <v-card class="d-flex align-center mb-4" rounded="lg" :min-height=height>
+  <v-card class="d-flex align-center mb-4" rounded="lg" :min-height="height">
     <v-row no-gutters class="fill-height" align="center" justify="center">
       <v-col v-if="build.civ" cols="3" class="pa-0 ma-0">
         <v-img
-        :min-height=height
+          :min-height="height"
           :src="
             '/' +
             civs.find((item) => {
@@ -32,7 +32,7 @@
       </v-col>
       <v-col v-if="!build.civ" cols="3" class="pa-0 ma-0">
         <v-img
-        :min-height=height
+          :min-height="height"
           src="/assets/flags/any-large.png"
           lazy-src="/assets/flags/any-small.png"
           gradient="to right, transparent, #222222"
@@ -83,12 +83,12 @@
             class="mr-2"
             label
             size="x-small"
-            v-show="build.views && orderBy == 'views' || 'score'"
+            v-show="build.views && (orderBy == 'views' || 'score')"
           >
             <v-icon start icon="mdi-eye"></v-icon>{{ build.views }}</v-chip
           >
           <v-chip
-            v-show="build.likes && orderBy == 'likes' || 'score'"
+            v-show="build.likes > 0 && (orderBy == 'likes' || 'score')"
             class="mr-2"
             label
             size="x-small"
@@ -136,12 +136,12 @@
             class="mr-2 mb-2"
             label
             size="small"
-            v-show="build.views && orderBy == 'views' || 'score'"
+            v-show="build.views && (orderBy == 'views' || 'score')"
           >
             <v-icon start icon="mdi-eye"></v-icon>{{ build.views }}</v-chip
           >
           <v-chip
-            v-show="build.likes && orderBy == 'likes' || 'score'"
+            v-show="build.likes && (orderBy == 'likes' || 'score')"
             class="mr-2 mb-2"
             label
             size="small"
@@ -169,7 +169,7 @@ import getCivs from "../composables/getCivs";
 import useTimeSince from "../composables/useTimeSince";
 import { useStore } from "vuex";
 import { computed, getCurrentInstance } from "vue";
-import { useDisplay } from 'vuetify'
+import { useDisplay } from "vuetify";
 
 export default {
   name: "SingleBuild",
@@ -177,20 +177,26 @@ export default {
   setup() {
     const civs = getCivs().civs;
     const store = useStore();
-    const { name } = useDisplay()
+    const { name } = useDisplay();
     const orderBy = computed(() => store.state.filterConfig.orderBy);
     const { timeSince, isNew } = useTimeSince();
 
     const height = computed(() => {
-        switch (name.value) {
-          case 'xs': return 60
-          case 'sm': return 60
-          case 'md': return 60
-          case 'lg': return 112
-          case 'xl': return 112
-          case 'xxl': return 112
-        }
-      })
+      switch (name.value) {
+        case "xs":
+          return 60;
+        case "sm":
+          return 60;
+        case "md":
+          return 60;
+        case "lg":
+          return 112;
+        case "xl":
+          return 112;
+        case "xxl":
+          return 112;
+      }
+    });
 
     return {
       civs,
