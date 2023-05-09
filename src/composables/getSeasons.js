bc99bd4400dc03fc835bf15b6dd03beec116b9fa
Jens Buehl <jbuehl@itemis.com>
2023-05-09 08:49:14 +0200

    Add season 5 skeleton and make it the default for new BOs

--- src/composables/getSeasons.js
+++ src/composables/getSeasons.js
@@ -2,14 +2,15 @@ import { ref } from "vue";
 
 const seasons = ref([
   {
-    title: "Season 1",
-    tagline: "Festival of Ages",
-    startDate: new Date(2022, 3, 7)
+    //TODO: Add tagline and date once info is available
+    title: "Season 5",
+    tagline: "",
+    startDate: new Date()
   },
   {
-    title: "Season 2",
-    tagline: "Map Monsters",
-    startDate: new Date(2022, 6, 12)
+    title: "Season 4",
+    tagline: "Enchanted Grove",
+    startDate: new Date(2023, 1, 16)
   },
   {
     title: "Season 3",
@@ -17,9 +18,14 @@ const seasons = ref([
     startDate: new Date(2022, 9, 25)
   },
   {
-    title: "Season 4",
-    tagline: "Enchanted Grove",
-    startDate: new Date(2023, 1, 16)
+    title: "Season 2",
+    tagline: "Map Monsters",
+    startDate: new Date(2022, 6, 12)
+  },
+  {
+    title: "Season 1",
+    tagline: "Festival of Ages",
+    startDate: new Date(2022, 3, 7)
   },
 ]);
 
