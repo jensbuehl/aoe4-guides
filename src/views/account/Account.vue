e4ec40e91dd2522f6ce58b05d6123c72ccd281ea
Jens Buehl <jbuehl@itemis.com>
2023-05-21 10:09:42 +0200

    Update mobile layout of account page

--- src/views/account/Account.vue
+++ src/views/account/Account.vue
@@ -1,103 +1,126 @@
 <template>
   <v-container>
-    <v-row class="justify-center">
-      <v-col>
-        <div v-if="authIsReady && user" class="d-flex justify-center">
-          <v-card rounded="lg">
-            <v-card-title class="mb-4">Account Info</v-card-title>
-            <v-card rounded="lg" style="max-width: 350px; width: 350px" fluid>
-              <v-text-field
-                class="text-grey"
-                name="displayname"
-                label="Display name"
-                type="text"
-                v-model="user.displayName"
-                placeholder="Your display name"
-                readonly
-              ></v-text-field>
-              <v-text-field
-                class="text-grey"
-                name="email"
-                label="Email"
-                type="email"
-                v-model="user.email"
-                placeholder="Your email"
-                readonly
-              ></v-text-field>
-              <v-text-field
-                class="text-grey"
-                name="user id"
-                label="User ID"
-                type="text"
-                v-model="user.uid"
-                placeholder="Your user id"
-                readonly
-              ></v-text-field>
-              <v-card v-if="error" rounded="lg" color="error">
-                <v-card-text>{{ error }}</v-card-text>
-              </v-card>
-            </v-card>
-          </v-card>
-        </div>
-        <div class="d-flex justify-center mt-4">
-          <v-card rounded="lg">
-            <v-card-title class="mb-4">Change Password</v-card-title>
-            <v-card rounded="lg" style="max-width: 350px; width: 350px" fluid>
-              <v-form ref="form" @submit.prevent="changePassword()">
+    <div v-if="authIsReady && user" class="d-flex justify-center">
+      <v-row no-gutters class="fill-height" align="center" justify="center">
+        <v-col cols="12" sm="6" lg="4">
+          <v-card rounded="lg" class="d-flex align-center mb-4">
+            <v-row
+              no-gutters
+              class="fill-height"
+              align="center"
+              justify="center"
+            >
+              <v-col cols="12">
+                <v-card-title class="mb-4">Account Info</v-card-title>
+              </v-col>
+              <v-col cols="12">
                 <v-text-field
-                  name="new password"
-                  label="New password"
-                  type="password"
-                  :rules="[(v) => !!v || 'Password is required']"
-                  v-model="newPassword"
-                  placeholder="Your new password"
-                  required
+                  class="text-grey"
+                  name="displayname"
+                  label="Display name"
+                  type="text"
+                  v-model="user.displayName"
+                  placeholder="Your display name"
+                  readonly
                 ></v-text-field>
-                <v-btn
-                  color="primary"
-                  variant="text"
-                  type="submit"
-                  block
-                  class="mt-2"
-                  >Change Password</v-btn
-                >
-              </v-form>
-            </v-card>
+                <v-text-field
+                  class="text-grey"
+                  name="email"
+                  label="Email"
+                  type="email"
+                  v-model="user.email"
+                  placeholder="Your email"
+                  readonly
+                ></v-text-field>
+                <v-text-field
+                  class="text-grey"
+                  name="user id"
+                  label="User ID"
+                  type="text"
+                  v-model="user.uid"
+                  placeholder="Your user id"
+                  readonly
+                ></v-text-field>
+                <v-card v-if="error" rounded="lg" color="error">
+                  <v-card-text>{{ error }}</v-card-text>
+                </v-card>
+              </v-col>
+            </v-row>
           </v-card>
-        </div>
-        <div class="d-flex justify-center">
-          <v-card rounded="lg" class="mt-4">
-            <v-card-title class="mb-4">Delete Account</v-card-title>
-            <v-card rounded="lg" style="max-width: 350px; width: 350px" fluid>
-              <v-form ref="form" @submit.prevent="dialog = true">
-                <v-btn
-                  color="primary"
-                  variant="text"
-                  type="submit"
-                  block
-                  class="mt-2"
-                  >Delete Account</v-btn
-                >
-                <v-dialog v-model="dialog" width="auto">
-                  <v-card rounded="lg" class="text-center primary">
-                    <v-card-title>Delete Account</v-card-title>
-                    <v-card-text>
-                      Do you really want to delete your account?<br />
-                      The action cannot be undone.
-                    </v-card-text>
-                    <v-card-actions>
-                      <v-btn color="error" block @click="deleteAccount()"
-                        >Delete</v-btn
-                      >
-                    </v-card-actions>
-                  </v-card>
-                </v-dialog>
-              </v-form>
-            </v-card>
+          <v-card rounded="lg" class="d-flex align-center mb-4">
+            <v-row
+              no-gutters
+              class="fill-height"
+              align="center"
+              justify="center"
+            >
+              <v-col cols="12">
+                <v-card-title class="mb-4">Change Password</v-card-title>
+              </v-col>
+              <v-col cols="12">
+                <v-form ref="form" @submit.prevent="changePassword()">
+                  <v-text-field
+                    name="new password"
+                    label="New password"
+                    type="password"
+                    :rules="[(v) => !!v || 'Password is required']"
+                    v-model="newPassword"
+                    placeholder="Your new password"
+                    required
+                  ></v-text-field>
+                  <v-btn
+                    color="primary"
+                    variant="text"
+                    type="submit"
+                    block
+                    class="mt-2"
+                    >Change Password</v-btn
+                  >
+                </v-form>
+              </v-col>
+            </v-row>
           </v-card>
-        </div>
-      </v-col>
-    </v-row>
+          <v-card rounded="lg" class="d-flex align-center mb-4">
+            <v-row
+              no-gutters
+              class="fill-height"
+              align="center"
+              justify="center"
+            >
+              <v-col cols="12">
+                <v-card-title class="mb-4">Delete Account</v-card-title>
+              </v-col>
+              <v-col cols="12">
+                <v-form ref="form" @submit.prevent="dialog = true">
+                  <v-btn
+                    color="primary"
+                    variant="text"
+                    type="submit"
+                    block
+                    class="mt-2"
+                    >Delete Account</v-btn
+                  >
+                  <v-dialog v-model="dialog" width="auto">
+                    <v-card rounded="lg" class="text-center primary">
+                      <v-card-title>Delete Account</v-card-title>
+                      <v-card-text>
+                        Do you really want to delete your account?<br />
+                        The action cannot be undone.
+                      </v-card-text>
+                      <v-card-actions>
+                        <v-btn color="error" block @click="deleteAccount()"
+                          >Delete</v-btn
+                        >
+                      </v-card-actions>
+                    </v-card>
+                  </v-dialog>
+                </v-form>
+              </v-col>
+            </v-row>
+          </v-card>
+        </v-col>
+      </v-row>
+    </div>
   </v-container>
 </template>
 
