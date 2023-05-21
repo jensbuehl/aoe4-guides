c2189b7b2970c9cf012756a3fb88d7e87719b318
Jens Buehl <jbuehl@itemis.com>
2023-05-21 10:21:13 +0200

    Update mobile layout of reset page

--- src/views/account/ResetPassword.vue
+++ src/views/account/ResetPassword.vue
@@ -1,31 +1,46 @@
 <template>
   <v-container>
     <div class="d-flex justify-center">
-      <v-card
-        class="elevation-10"
-        rounded="lg"
-        style="max-width: 350px; width: 350px"
-        fluid
+      <v-row no-gutters class="fill-height" align="center" justify="center">
+        <v-col cols="12" sm="6" lg="4">
+          <v-card rounded="lg" class="d-flex align-center mb-4">
+            <v-row
+              no-gutters
+              class="fill-height"
+              align="center"
+              justify="center"
+            >
+              <v-col cols="12">
+                <v-card-title class="mb-4">Reset Password</v-card-title>
+              </v-col>
+              <v-col cols="12">
+                <v-form ref="form" @submit.prevent="reset()">
+                  <v-text-field
+                    v-model="email"
+                    name="email"
+                    label="E-mail"
+                    type="email"
+                    placeholder="Your e-mail"
+                    :rules="[(v) => !!v || 'Email is required']"
+                    required
+                  ></v-text-field>
+                  <v-btn
+                    variant="text"
+                    color="primary"
+                    type="submit"
+                    block
+                    class="mt-2"
+                    >Reset Password</v-btn
+                  >
+                  <v-card v-if="error" rounded="lg" color="error">
+                    <v-card-text>{{ error }}</v-card-text>
+                  </v-card>
+                </v-form>
+              </v-col>
+            </v-row>
+          </v-card></v-col
+        ></v-row
       >
-        <v-card-title class="mb-4">Reset Password</v-card-title>
-        <v-form ref="form" @submit.prevent="reset()">
-          <v-text-field
-            v-model="email"
-            name="email"
-            label="E-mail"
-            type="email"
-            placeholder="Your e-mail"
-            :rules="[v => !!v || 'Email is required']"
-            required
-          ></v-text-field>
-          <v-btn variant="text" color="primary" type="submit" block class="mt-2"
-            >Reset Password</v-btn
-          >
-          <v-card v-if="error" rounded="lg" color="error">
-            <v-card-text>{{ error }}</v-card-text>
-          </v-card>
-        </v-form>
-      </v-card>
     </div>
   </v-container>
 </template>
@@ -42,23 +57,28 @@ export default {
     const email = ref("");
     const router = useRouter();
     const error = ref(null);
+    const form = ref(null);
 
     const actionCodeSettings = {
       url: "https://aoe4guides.com/login",
     };
     const reset = async () => {
+      const validation = await form.value.validate();
+      if (!validation.valid) return;
+
       await sendPasswordResetEmail(auth, email.value, actionCodeSettings)
         .then(() => {
           alert("Password reset email sent!");
           router.push("/");
         })
         .catch((err) => {
-          error.value =  "Could not reset password. " + err.code;
+          error.value = "Could not reset password. " + err.code;
         });
     };
     return {
       error,
       email,
+      form,
       reset,
     };
   },
