273e9918ad215626fca700ed1b28a1d0362db00c
Jens Bühl <jbuehl@itemis.com>
2023-07-17 14:37:34 +0200

    Comment regarding cleaning up deprecated elements in the user's favorites table

--- src/views/builds/BuildDetails.vue
+++ src/views/builds/BuildDetails.vue
@@ -773,6 +773,8 @@ export default {
     const handleDelete = async () => {
       dialog.value = false;
       await del(props.id);
+      //TODO: if favorites count != received favorites count, then update favorites list in DB (remove deprecated links)
+      //Preferably, fix in cloud function, whenever a build is removed.
       if (!error.value) {
         router.push({ name: "Home" });
       }
