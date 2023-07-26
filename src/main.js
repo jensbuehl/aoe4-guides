45b0f55000f0e39607d25c673358458341335c1c
Jens Bühl <jbuehl@itemis.com>
2023-07-26 14:19:15 +0200

    Prepare for light theme

--- src/main.js
+++ src/main.js
@@ -1,48 +1,67 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import { createVuetify } from 'vuetify'
-import router from './router/'
+import { createApp } from "vue";
+import App from "./App.vue";
+import { createVuetify } from "vuetify";
+import router from "./router/";
 
 // global styles and icons
-import './assets/main.css'
-import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/dist/vuetify.min.css'
+import "./assets/main.css";
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/dist/vuetify.min.css";
 
 // import store
-import store from './store'
+import store from "./store";
 
 // theme definition
 const customDarkTheme = {
   dark: true,
   colors: {
-    accent: '#e7c05e',
-    anchor: '#e7c05e',
-    background: '#161925',
-    surface: '#1D2432',
-    'surface-variant': '#47587F', //tooltips
-    primary: '#e7c05e',
-    'primary-darken-1': '#cca741',
-    secondary: '#e7c05e',
-    'secondary-darken-1': '#cca741',
+    accent: "#e7c05e",
+    anchor: "#e7c05e",
+    //background: '#161925',
+    surface: "#1D2432",
+    "surface-variant": "#47587F", //tooltips
+    primary: "#e7c05e",
+    "primary-darken-1": "#cca741",
+    secondary: "#e7c05e",
+    "secondary-darken-1": "#cca741",
     //success: '#e7c05e',
     //warning: '#e7c05e',
     //error: '#e7c05e',
-    info: '#e7c05e',
-  }
-}
+    info: "#e7c05e",
+  },
+};
+
+const customLightTheme = {
+  dark: false,
+  colors: {
+    accent: "#e7c05e",
+    anchor: "#e7c05e",
+    //background: '#161925',
+    surface: "#1D2432",
+    "surface-variant": "#47587F", //tooltips
+    primary: "#e7c05e",
+    "primary-darken-1": "#cca741",
+    secondary: "#e7c05e",
+    "secondary-darken-1": "#cca741",
+    //success: '#e7c05e',
+    //warning: '#e7c05e',
+    //error: '#e7c05e',
+    info: "#e7c05e",
+  },
+};
 
 // init app
 const vuetify = createVuetify({
-  components: {
-  },
+  components: {},
   theme: {
     options: {
-      customProperties: true
+      customProperties: true,
     },
-    defaultTheme: 'customDarkTheme',
+    defaultTheme: "customDarkTheme",
     themes: {
-      customDarkTheme
-    }
-  }
-})
-createApp(App).use(vuetify).use(router).use(store).mount('#app');
+      customDarkTheme,
+      customLightTheme
+    },
+  },
+});
+createApp(App).use(vuetify).use(router).use(store).mount("#app");
