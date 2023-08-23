d607b1fb78c23b440e4d18d722685a2a63f08d9a
Jens Bühl <jbuehl@itemis.com>
2023-08-23 08:46:05 +0200

    Fix column width sync

--- src/components/builds/StepsEditor.vue
+++ src/components/builds/StepsEditor.vue
@@ -159,6 +159,7 @@ export default {
         age: currentAge + 1,
         steps: [{}],
       });
+      alignTableColumnWidth("align-to-widest");
       context.emit("stepsChanged", sections);
     };
 
