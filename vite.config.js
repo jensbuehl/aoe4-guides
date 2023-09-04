354f3e933ba2805e85281a5178ac06407fa174e6
Jens Bühl <jbuehl@itemis.com>
2023-09-04 13:56:32 +0200

    Update PWA manifest data

--- vite.config.js
+++ vite.config.js
@@ -17,11 +17,10 @@ export default {
   ],
   resolve: {},
   manaifest: {
-    name: "Test",
-    short_name: "Test_Short",
-    description: "Description",
-    theme_color: "#ff0000",
-    background_color: "#000000",
+    name: "Age of Empires IV Build Orders | AOE4 GUIDES",
+    short_name: "AOE4 GUIDES",
+    description: "Create and share build orders for Age of Empires IV. Export and use any build order with the Overlay Tool for AoE4.",
+    theme_color: "#e7c05e",
     display: "standalone",
     icons: [
       {
