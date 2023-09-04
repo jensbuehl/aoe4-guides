4380c8b728e67d2289a4e58f37a10293496301b1
Jens Bühl <jbuehl@itemis.com>
2023-09-04 17:53:04 +0200

    Update background color and app name

--- vite.config.js
+++ vite.config.js
@@ -14,11 +14,11 @@ export default {
         globPatterns: ['**/*.{js,css,html,ico,png,svg,json,vue,txt,woff2}']
       },
       manifest: {
-        name: "Age of Empires IV Build Orders | AOE4 GUIDES",
+        name: "AOE4 GUIDES",
         short_name: "AOE4 GUIDES",
         description: "Create and share build orders for Age of Empires IV. Export and use any build order with the Overlay Tool for AoE4.",
         theme_color: "#e7c05e",
-        background_color: "#000000",
+        background_color: "#1D2432",
         display: "standalone",
         icons: [
           {
