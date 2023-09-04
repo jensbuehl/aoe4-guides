a43debb25fc18bf4384045c3d1483ff4bc71dc89
Jens Bühl <jbuehl@itemis.com>
2023-09-04 17:49:21 +0200

    Fix PWA configuration

--- vite.config.js
+++ vite.config.js
@@ -12,28 +12,29 @@ export default {
       workbox: {
         cleanupOutdatedCaches: false,
         globPatterns: ['**/*.{js,css,html,ico,png,svg,json,vue,txt,woff2}']
-      }
+      },
+      manifest: {
+        name: "Age of Empires IV Build Orders | AOE4 GUIDES",
+        short_name: "AOE4 GUIDES",
+        description: "Create and share build orders for Age of Empires IV. Export and use any build order with the Overlay Tool for AoE4.",
+        theme_color: "#e7c05e",
+        background_color: "#000000",
+        display: "standalone",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
     }),
   ],
   resolve: {},
-  manaifest: {
-    name: "Age of Empires IV Build Orders | AOE4 GUIDES",
-    short_name: "AOE4 GUIDES",
-    description: "Create and share build orders for Age of Empires IV. Export and use any build order with the Overlay Tool for AoE4.",
-    theme_color: "#e7c05e",
-    display: "standalone",
-    icons: [
-      {
-        src: "/android-chrome-192x192.png",
-        sizes: "192x192",
-        type: "image/png",
-      },
-      {
-        src: "/android-chrome-512x512.png",
-        sizes: "512x512",
-        type: "image/png",
-      },
-    ],
-  },
   build: { chunkSizeWarningLimit: 1600 },
 };
