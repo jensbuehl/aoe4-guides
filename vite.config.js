bd866ba98c8350ca6a47919724c0b9323655852c
Jens Bühl <jbuehl@itemis.com>
2023-09-26 08:28:06 +0200

    Revert "PWA"
    This reverts commit 1a736348a9d90c6afbc0ab145d6c55c35e16f424.

--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,14 @@
-import vue from "@vitejs/plugin-vue";
-import vuetify from "vite-plugin-vuetify";
-import { VitePWA } from "vite-plugin-pwa";
+import vue from '@vitejs/plugin-vue'
+import vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
 export default {
   plugins: [
     vue(),
     vuetify(),
-    VitePWA({
-      registerType: "autoUpdate",
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,json,vue,txt,woff2}']
-      },
-      manifest: {
-        name: "AOE4 GUIDES",
-        short_name: "AOE4 GUIDES",
-        description: "Create and share build orders for Age of Empires IV. Export and use any build order with the Overlay Tool for AoE4.",
-        background_color: "#1D2432",
-        display: "standalone",
-        icons: [
-          {
-            src: "/android-chrome-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "/android-chrome-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-      },
-    }),
-  ],
-  resolve: {},
-  build: { chunkSizeWarningLimit: 1600 },
-};
+    ],
+  resolve: {
+
+  },
+  build: { chunkSizeWarningLimit: 1600, }
+}
