6f46d192eb89643b936c60382889b937d42605a0
Jens Bühl <jbuehl@itemis.com>
2023-11-17 17:17:08 +0100

    Add hyperlink to ko-fi

--- src/components/NewsCard.vue
+++ src/components/NewsCard.vue
@@ -11,18 +11,20 @@
 
       <v-row class="ma-2" no-gutters>
         <v-col align="center" justify="center"
-          ><v-btn
-            class="ma-2"
-            size="small"
-            variant="plain"
-            color="primary"
-            style="background-color: transparent"
-          >
-            <template v-slot:prepend>
-              <v-icon color="red">mdi-heart</v-icon>
-            </template>
-            Support me
-          </v-btn></v-col
+          ><a href="https://ko-fi.com/jensbuehl"
+            ><v-btn
+              class="ma-2"
+              size="small"
+              variant="plain"
+              color="primary"
+              style="background-color: transparent"
+            >
+              <template v-slot:prepend>
+                <v-icon color="red">mdi-heart</v-icon>
+              </template>
+              Support me
+            </v-btn></a
+          ></v-col
         >
       </v-row>
     </v-card></v-alert
