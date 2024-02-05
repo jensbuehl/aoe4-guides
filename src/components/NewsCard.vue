79a95aa26c8bd0d243a00f00bdcdad22a5fdfdbf
Jens Bühl <jbuehl@itemis.com>
2024-02-05 12:19:49 +0100

    Update news card

--- src/components/NewsCard.vue
+++ src/components/NewsCard.vue
@@ -1,11 +1,52 @@
 <template>
   <v-alert rounded="lg" outlined color="accent" class="pa-1">
     <v-card flat rounded="lg">
-      <v-card-title>Happy New Year!</v-card-title>
+      <v-card-title>New Year, new patch!</v-card-title>
       <v-card-text
-        >The village is growing! More than 500 guides, approaching 1k registered villagers, thousands of views! <br /><br />
-        If you want to support the project, a new funding goal for 2024 is now live for olive oil and coffee! Happy scouting!</v-card-text
-      >
+        >The new Japanese technologies are now available. <br /><br />
+        <v-row no-gutters>
+          <v-col align="center" justify="center"
+            ><v-img
+              class="icon-tech-selector"
+              src="/assets/pictures/technology_japanese/shinto_rituals.png"
+            ></v-img
+          ></v-col>
+          <v-col align="center" justify="center"
+            ><v-img
+            class="icon-tech-selector"
+              src="/assets/pictures/technology_japanese/gion_festival.png"
+            ></v-img
+          ></v-col>
+          <v-col align="center" justify="center"
+            ><v-img
+            class="icon-tech-selector"
+              src="/assets/pictures/technology_japanese/bunrei.png"
+            ></v-img
+          ></v-col>
+          <v-col align="center" justify="center"
+            ><v-img
+            class="icon-tech-selector"
+              src="/assets/pictures/technology_japanese/zen.png"
+            ></v-img
+          ></v-col>
+          <v-col align="center" justify="center"
+            ><v-img
+            class="icon-tech-selector"
+              src="/assets/pictures/technology_japanese/nehan.png"
+            ></v-img
+          ></v-col>
+          <v-col align="center" justify="center"
+            ><v-img
+            class="icon-tech-selector"
+              src="/assets/pictures/technology_japanese/five_ministries.png"
+            ></v-img
+          ></v-col>
+        </v-row><br /><br />
+        If you are a happy villager, please consider supporting to cover the
+        operational costs! <br /><br />
+        A funding goal for 2024 is now live for olive oil and coffee! Happy
+        scouting!<br /><br />
+      </v-card-text>
 
       <v-row class="ma-2" no-gutters>
         <v-col align="center" justify="center"
