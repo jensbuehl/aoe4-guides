338be6d13ef144b1db4e3d9bd66788cd387d2db0
Jens Bühl <jbuehl@itemis.com>
2024-03-07 09:26:52 +0100

    Align style of 404 page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,10 +1,28 @@
 <template>
-    <v-container>
-      <div class="d-flex justify-center">
-        <v-card flat class="text-center" rounded="lg" style="max-width: 350px; width: 350px" fluid>
-          <v-card-title class="mb-4">404</v-card-title>
-          <v-card-text>Page not found</v-card-text>
-        </v-card>
-      </div>
-    </v-container>
-  </template>
+  <v-container>
+    <v-alert rounded="lg" outlined color="accent" class="pa-1">
+      <v-card flat rounded="lg" align="center">
+        <v-card-title>404</v-card-title>
+        <v-card-text>Page not found<br /> </v-card-text>
+
+        <v-row class="ma-2" no-gutters justify="center">
+          <v-btn
+            color="primary"
+            style="background-color: transparent"
+            variant="text"
+            to="/"
+          >
+            Home
+          </v-btn>
+        </v-row>
+      </v-card></v-alert
+    >
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "BuildNotFound",
+  setup() {},
+};
+</script>
