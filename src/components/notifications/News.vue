5b87fc1593144354fac51763320a15f8bc25cac2
Jens Bühl <jbuehl@itemis.com>
2024-05-10 14:00:40 +0200

    1000 BOs celebration news

--- src/components/notifications/News.vue
+++ src/components/notifications/News.vue
@@ -1,22 +1,15 @@
 <template>
   <v-alert rounded="lg" outlined color="accent" class="pa-1">
     <v-card flat rounded="lg">
-      <v-card-title>Improve Faster!</v-card-title>
+      <v-card-title>1000 Build Orders Strong!!</v-card-title>
       <v-card-text>
-        We're excited to unveil a new update, powered by the rich data from <b>AoE4 World</b>! Thanks to the AoE4 World team for providing us with the detailed insights that make this enhancement possible.<br><br>
-        <b>Enhanced Tooltips:</b>
-        Get detailed descriptions and costs for units and buildings directly in tooltips, making
-        your strategizing more informed and efficient.<br><br>
+        We've just hit a major milestone at aoe4guides.com: Over 1000 community-crafted build orders! It's been quite the year, with our fair share of traffic spikes and loads of new features rolling out thanks to your feedback and contributions.<br /><br />
         
-        <b>Direct aoe4world Exploration:</b>
-        Curious about a unit or building? Click on their images to dive into aoe4world for comprehensive
-        stats and insights.<br><br>
-                
-        <b>Autocomplete</b>: For those who haven't explored it yet, the "autocomplete"
-        continues to streamline your build order crafting process. Just try typing '::' and see
-        what happens!<br><br>
+        A huge thank you to all of you for making this site a treasure trove of strategies for fellow fans of Age of Empires IV like myself. Your insights and dedication have shaped aoe4guides.com into a fantastic resource that keeps getting better.<br /><br />
 
-        Thanks for your support and happy scouting!
+        Thanks for your support and happy scouting! Here's to many more games, guides, and victories! Keep those build orders coming!
+
+        -exe
       </v-card-text>
 
       <v-row class="ma-2" no-gutters>
@@ -38,41 +31,7 @@
         >
       </v-row>
     </v-card>
-    <!--v-card flat rounded="lg">
-      <v-card-title>Autocomplete & Notes!</v-card-title>
-      <v-card-text>
-        We're thrilled to introduce a new feature for all villagers: The ability to add notes to
-        each build order section. <br /><br />
-        This addition allows you to annotate your strategies with tips, reminders, or alternative
-        tactics, ensuring that your game plan is as detailed and adaptable as possible.<br /><br />
-
-        And for those who haven't explored it yet, the "autocomplete" feature continues to
-        streamline your build order crafting process.
-
-        <v-img class="ma-8" src="/autocomplete.gif"></v-img>
-        Thanks for your support and happy scouting!
-      </v-card-text>
-
-      <v-row class="ma-2" no-gutters>
-        <v-col align="center" justify="center"
-          ><a href="https://ko-fi.com/jensbuehl"
-            ><v-btn
-              class="ma-2"
-              size="small"
-              variant="plain"
-              color="primary"
-              style="background-color: transparent"
-            >
-              <template v-slot:prepend>
-                <v-icon color="red">mdi-heart</v-icon>
-              </template>
-              Support me
-            </v-btn></a
-          ></v-col
-        >
-      </v-row>
-    </v-card--></v-alert
-  >
+  </v-alert>
 </template>
 
 <script>
