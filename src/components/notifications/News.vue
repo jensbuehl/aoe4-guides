f0709004e4fea6848b5b186b78494e54cf3cb59f
Jens Bühl <jbuehl@itemis.com>
2024-07-29 21:27:17 +0200

    Update news card (Gold3Dad)

--- src/components/notifications/News.vue
+++ src/components/notifications/News.vue
@@ -1,19 +1,43 @@
 <template>
   <v-alert rounded="lg" outlined color="accent" class="pa-1">
     <v-card flat rounded="lg">
-      <v-card-title>1000 Build Orders Strong!!</v-card-title>
+      <v-card-title>Sounds of Victory</v-card-title>
       <v-card-text>
-        We've just hit a major milestone at aoe4guides.com: Over 1000 community-crafted build orders! It's been quite the year, with our fair share of traffic spikes and loads of new features rolling out thanks to your feedback and contributions.<br /><br />
-        
-        A huge thank you to all of you for making this site a treasure trove of strategies for fellow fans of Age of Empires IV like myself. Your insights and dedication have shaped aoe4guides.com into a fantastic resource that keeps getting better.<br /><br />
+        There's a new YouTube channel showcasing community-created songs inspired by Age of
+        Empires IV. <br /><br />
 
-        Thanks for your support and happy scouting!<br>
+        If you're interested in hearing unique takes on the game's themes, check it out.
+        <br /><br />
+
+        More content may come in the future, so stay tuned. Good luck, have fun!<br /><br />
 
         -exe
+
+        <div align="center">
+          <iframe
+            width="100%"
+            height="200px"
+            src="https://www.youtube.com/embed/9u3eYzxSyE0"
+            frameborder="0"
+            class="my-5"
+            allowfullscreen
+          ></iframe>
+        </div>
       </v-card-text>
 
       <v-row class="ma-2" no-gutters>
         <v-col align="center" justify="center"
+          ><a href="https://www.youtube.com/channel/UC5hqMmEWvSyFgsvQb29clMw/videos"
+            ><v-btn class="ma-2"
+              size="small"
+              variant="plain"
+              color="primary"
+              style="background-color: transparent">
+              <template v-slot:prepend>
+                <v-icon color="red">mdi-youtube</v-icon>
+              </template>
+              Check it out
+            </v-btn></a
           ><a href="https://ko-fi.com/jensbuehl"
             ><v-btn
               class="ma-2"
