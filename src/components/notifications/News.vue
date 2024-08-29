883492836862ed86413d6e2df43af5e45e1780c9
Jens Bühl <jbuehl@itemis.com>
2024-08-29 14:56:10 +0200

    News card update

--- src/components/notifications/News.vue
+++ src/components/notifications/News.vue
@@ -3,21 +3,22 @@
     <v-card flat rounded="lg">
       <v-card-title>Happy Birthday, Schnitz!</v-card-title>
       <v-card-text>
-        <p class="mt-2 mb-4">
-          Today would have been the birthday of my best friend, Schnitz, who passed away two years
-          ago. Most of you might not have known him, but he was a huge part of my life.
+        <p class="mt-2 mb-4">August 30, 2024</p>
+        <p class="mb-4">
+          Today would have been the birthday of my best friend, Schnitz. Most of you might not have
+          known him, but he was a huge part of my life and was the trigger for me to start this
+          project. He passed away two years ago.
         </p>
 
         <p class="mb-4">
-          Schnitz and I shared a deep passion for gaming, especially Age of Empires. Our time
-          together, particularly in those games, inspired me to start AoE4 Guides, both as a tribute
-          to him and as a way to keep myself occupied after his loss.
+          Schnitz and I shared a deep passion for gaming, especially Age of Empires. Common
+          memories, and the shared enthusiasm for for those games, inspired me to start AoE4 Guides,
+          both as a tribute to him and as a way to keep myself occupied after his loss.
         </p>
 
         <p class="mb-4">
-          In honor of his memory, I've created a song that captures some of the moments we shared.
-          If you’d like to see what he meant to me, you can listen to it here (and learn some
-          German):
+          In honor of his memory, I've written down a few words that capture some of the moments we
+          shared. If you’d like to see what he meant to me, you can listen to it here (unfortunately, it's only available in German):<br>
           <a
             :style="{
               color: $vuetify.theme.current.colors.primary,
@@ -25,13 +26,13 @@
             href="https://www.youtube.com/watch?v=yVKWWyy7a-8"
             target="_blank"
             >Du Warst Da</a
-          >.
+          >
         </p>
 
         <p class="mb-4">
-          Also, I want to thank this incredible community for helping shape AoE4 Guides and the AoE4
-          community into what it is today. Your support has meant the world to me, and I know he
-          would have been proud of what we’ve built together.
+          Also, I want to thank all of you for helping shape AoE4 Guides and the AoE4 community into
+          what it is today. Your support helped me a lot, and I know he would have been proud of
+          what we’ve built together.
         </p>
 
         <p class="mb-4">Thank you for letting me share this with you.</p>
