2a2ee60fa0c7af5f060e28a7af725c23b4a1a89b
Jens Bühl <jbuehl@itemis.com>
2024-09-02 12:11:46 +0200

    Replace news card

--- src/components/notifications/News.vue
+++ src/components/notifications/News.vue
@@ -1,43 +1,18 @@
 <template>
   <v-alert rounded="lg" outlined color="accent" class="pa-1 mt-6 mb-2">
     <v-card flat rounded="lg">
-      <v-card-title>Happy Birthday, Schnitz!</v-card-title>
+      <v-card-title>Ready for AoM:R?</v-card-title>
       <v-card-text>
-        <p class="mt-2 mb-4">August 30, 2024</p>
         <p class="mb-4">
-          Today would have been the birthday of my best friend, Schnitz. Most of you might not have
-          known him, but he was a huge part of my life and was the trigger for me to start this
-          project. He passed away two years ago.
+          Give it a shot to help strengthen the entire Age of Empires community. Have a great remaining season 8!
         </p>
 
-        <p class="mb-4">
-          Schnitz and I shared a deep passion for gaming, especially Age of Empires. Common
-          memories, and the shared enthusiasm for for those games, inspired me to start AoE4 Guides,
-          both as a tribute to him and as a way to keep myself occupied after his loss.
+        <p class="mb-4">/3,<br />exe</p>
+
+        <p>
+          P.S. '<strong>::</strong>' is <strong>the shortcut</strong> to get your
+          builds done quicker!
         </p>
-
-        <p class="mb-4">
-          In honor of his memory, I've written down a few words that capture some of the moments we
-          shared. If you’d like to see what he meant to me, you can listen to it here (unfortunately, it's only available in German):<br>
-          <a
-            :style="{
-              color: $vuetify.theme.current.colors.primary,
-            }"
-            href="https://www.youtube.com/watch?v=yVKWWyy7a-8"
-            target="_blank"
-            >Du Warst Da</a
-          >
-        </p>
-
-        <p class="mb-4">
-          Also, I want to thank all of you for helping shape AoE4 Guides and the AoE4 community into
-          what it is today. Your support helped me a lot, and I know he would have been proud of
-          what we’ve built together.
-        </p>
-
-        <p class="mb-4">Thank you for letting me share this with you.</p>
-
-        <p>Sincerely,<br />exe</p>
       </v-card-text>
 
       <v-row class="mx-2 mb-2" no-gutters>
