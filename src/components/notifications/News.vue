f542380ab90aca53ae6025d4ee7cf0a21c848537
Jens Bühl <jbuehl@itemis.com>
2024-10-22 21:41:46 +0200

    Refined news text

--- src/components/notifications/News.vue
+++ src/components/notifications/News.vue
@@ -2,20 +2,51 @@
   <v-alert rounded="lg" outlined color="accent" class="pa-1 mt-6 mb-2">
     <v-card flat rounded="lg">
       <v-card-title>Season 9 Update</v-card-title>
-      <v-card-text>        
-        <p class="mb-4">
-          Hey AoE4 fans!
-        </p>
+      <v-card-text>
+        <p class="mb-4">Hey AoE4 fans!</p>
 
         <p class="mb-4">
-          Season 9 has dropped, and we've got some exciting updates. New technologies and units have entered the battlefield, shaking up strategies and adding depth to your gameplay. Get ready to experiment with fresh tactics and master these new additions! Stay tuned for updated guides, strategies, and tips to dominate in Season 9!
+          Season 9 has dropped, and we've got some exciting updates. New technologies and units have
+          entered the battlefield, shaking up strategies and adding depth to your gameplay. Get
+          ready to experiment with fresh tactics and master these new additions! <br /><br />
+          
+          Stay tuned for updated guides, strategies, and tips to climb the ladder in Season 9!
         </p>
+
+        <br />
+        <v-row no-gutters>
+          <v-col class="mb-2" align="center" justify="center"
+            ><v-img
+              class="icon-military-selector"
+              src="/assets/pictures/unit_malians/freeborn-mansa.png"
+            ></v-img
+          ></v-col>
+          <v-col class="mb-2" align="center" justify="center"
+            ><v-img
+              class="icon-military-selector"
+              src="/assets/pictures/unit_malians/javelin-thrower-mansa.png"
+            ></v-img
+          ></v-col>
+          <v-col class="mb-2" align="center" justify="center"
+            ><v-img
+              class="icon-military-selector"
+              src="/assets/pictures/unit_malians/musofadi-mansa.png"
+            ></v-img
+          ></v-col>
+          <v-col class="mb-2" align="center" justify="center"
+            ><v-img
+              class="icon-military-selector"
+              src="/assets/pictures/unit_rus/horse-archer-3.png"
+            ></v-img
+          ></v-col>
+        </v-row>
+        <br />
 
         <p class="mb-4">/3,<br />exe</p>
 
         <p>
-          P.S. '<strong>::</strong>' is <strong>the shortcut</strong> to get your
-          builds done quicker!
+          P.S. '<strong>::</strong>' is <strong>the shortcut</strong> to get your builds done
+          quicker!
         </p>
       </v-card-text>
 
