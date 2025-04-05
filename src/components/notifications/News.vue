af1b8d7e5738158f78b167f5fa01e74674d5656d
Jens Bühl <jbuehl@itemis.com>
2025-04-05 12:42:12 +0200

    Add news banner

--- src/components/notifications/News.vue
+++ src/components/notifications/News.vue
@@ -1,26 +1,24 @@
 <template>
   <v-alert rounded="lg" outlined color="accent" class="pa-1 mt-6 mb-2">
     <v-card flat rounded="lg">
-      <v-card-title>New Year | New DLC</v-card-title>
+      <v-img
+        color="surface-variant"
+        height="200"
+        src="/dlc-banner.webp"
+        cover
+      ></v-img>
+      <v-card-title>DLC Update | Now Live!</v-card-title>
       <v-card-text>
-        <p class="mb-4">Dear AoE4 community,</p>
-
         <p class="mb-2">
-        I want to thank you all for your continued support of Aoe4Guides. This platform has always been a labor of love, created to help players like you explore and master the rich strategies of <em>Age of Empires IV</em>.
+          Aoe4Guides just got a major update! All units, buildings, and techs from the new DLC
+          <strong>Knights of Cross and Rose</strong> are now added.
         </p>
-
         <p class="mb-2">
-        This year brings exciting challenges and opportunities, especially with the upcoming big DLC release. I’m committed to updating the site with fresh content. 
-        However, running Aoe4Guides comes with operational costs – hosting, traffic, and the effort it takes to keep things up to date.
-       </p>
-        
-       <p class="mb-2">
-        If you’ve found value in the guides, tips, or resources on this site, I’d greatly appreciate your support. Your contributions help cover operational costs and enable me to dedicate the time needed to keep Aoe4Guides the best resource for AoE4 build orders. Together, we can continue to grow and celebrate this incredible game.
-       </p>
-
-        <p class="mb-2">Thank you for being part of this journey. Here’s to a fantastic year ahead, full of epic battles, new civilizations, and a new DLC!</p>
-
-        <p class="mb-4">/3,<br />exe</p>
+          If you stumble across anything missing or broken - yeah, it happens - just let me know.
+        </p>
+        <p class="mb-2">If this update saved you some time or clicks, consider throwing a coin my way. It helps keep the site running.</p>
+        <p class="mb-2">Thanks for keeping the site alive and sharp. You're awesome. If </p>
+        <p class="mb-2">/3,<br />exe</p>
 
         <p>
           P.S. '<strong>::</strong>' is <strong>the shortcut</strong> to get your builds done
