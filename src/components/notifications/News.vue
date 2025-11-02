b70f47e6074d01870a077d5bc9210aa479e5bb39
Jens Bühl <jbuehl@itemis.com>
2025-11-02 17:54:23 +0100

    Update news page

--- src/components/notifications/News.vue
+++ src/components/notifications/News.vue
@@ -2,24 +2,65 @@
   <v-alert rounded="lg" outlined color="accent" class="pa-1 mt-6 mb-2">
     <v-card flat rounded="lg">
       <v-img color="surface-variant" src="/Season11-banner.webp"></v-img>
-      <v-card-title> Season 11 | WOLOLO </v-card-title>
+      <v-card-title>Dynasties of the East — Now Live!</v-card-title>
       <v-card-text>
         <p>
-          🚀 Season 11 of Age of Empires IV is here! We've rolled out performance improvements to
-          ensure your experience is smoother and faster than ever. Let’s celebrate with a big, booming WOLOLO!
+          🚀 The "Dynasties of the East" DLC update is live now and you can start using the four new
+          civilizations today: Golden Horde, Macedonian Dynasty, Sengoku Daimyo, and Tughlaq
+          Dynasty.
         </p>
 
+        <p class="mt-4">
+          More exciting information about the DLC and how to get it here:</p>
+
+        <v-row class="mx-2" no-gutters>
+          <v-col align="center" justify="center">
+            <a href="https://www.youtube.com/watch?v=f9lffIAztko" target="_blank">
+              <v-btn
+                class="ma-2"
+                size="small"
+                variant="text"
+                color="primary"
+                style="background-color: transparent"
+              >
+                <template v-slot:prepend>
+                  <v-icon color="primary">mdi-play-circle</v-icon>
+                </template>
+                Trailer
+              </v-btn>
+            </a>
+            <a href="https://www.ageofempires.com/games/age-of-empires-iv/dynasties-of-the-east/" target="_blank">
+              <v-btn
+                class="ma-2"
+                size="small"
+                variant="text"
+                color="primary"
+                style="background-color: transparent"
+              >
+                <template v-slot:prepend>
+                  <v-icon color="primary">mdi-web</v-icon>
+                </template>
+                Website
+              </v-btn>
+            </a>
+          </v-col>
+        </v-row>
+        
+        <div class="video-container mt-4">
+          <iframe
+          class="video"
+          :src="'https://www.youtube.com/embed/NFT1552oa1c'"
+          allowfullscreen
+          ></iframe>
+        </div>
         <p class="mt-4">
           📊 Thanks to your support, the last season was the most traffic-heavy to date!
         </p>
 
         <p class="mt-4">
-          💖 Want to help keep the momentum going? Consider supporting us on Ko-fi to help cover costs.
+          💖 If you'd like to support ongoing development and hosting costs, consider donating on
+          Ko-fi.
         </p>
-
-        <div class="video-container mt-4">
-          <iframe class="video" :src="'https://www.youtube.com/embed/YHIqfYOY5LU'"></iframe>
-        </div>
       </v-card-text>
 
       <v-row class="mx-2 mb-2" no-gutters>
