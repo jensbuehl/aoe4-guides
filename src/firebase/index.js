bcb86b5c917f50cc454cf02f868bf194bcb3a86e
Jens Bühl <jens.buehl@outlook.com>
2026-01-19 14:24:12 +0100

    fix: correct projectId typo in Firebase config and update dependencies
    - Fixed typo in projectId from VITE_FIREBASE_PROEJCT_ID to VITE_FIREBASE_PROJECT_ID in src/firebase/index.js.
    - Added firebase-tools and g packages to package.json.

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -38,7 +38,7 @@ import { getFunctions } from "firebase/functions";
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROEJCT_ID,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
